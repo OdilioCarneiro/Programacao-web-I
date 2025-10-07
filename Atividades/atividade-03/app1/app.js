de3e062a9931cd13e64823de2300de069947dac7
OdilioCarneiro <odilionetocarneironogueira@gmail.com>
2025-10-07 16:49:30 -0300

    colocar a pasta de app dentro de atividade-03

---
+++ Atividades/atividade-03/app1/app.js
@@ -0,0 +1,48 @@
+var createError = require('http-errors');
+var express = require('express');
+var path = require('path');
+var cookieParser = require('cookie-parser');
+var logger = require('morgan');
+
+var indexRouter = require('./routes/index');
+var usersRouter = require('./routes/users');
+
+var app = express();
+
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'ejs');
+
+app.use((req, res, next) => {
+  console.log('%s %s %s', req.method, req.url, req.path);
+  next();
+});
+
+app.use(logger('dev'));
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(cookieParser());
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.use('/', indexRouter);
+app.use('/users', usersRouter);
+app.use('/about', require('./routes/about'));
+app.use('/data', require('./routes/data'));
+
+
+
+app.use(function(req, res, next) {
+  next(createError(404));
+});
+
+
+app.use(function(err, req, res, next) {
+
+  res.locals.message = err.message;
+  res.locals.error = req.app.get('env') === 'development' ? err : {};
+
+
+  res.status(err.status || 500);
+  res.render('error');
+});
+
+module.exports = app;
---
+++ Atividades/atividade-03/app1/routes/users.js
@@ -0,0 +1,35 @@
+var express = require('express');
+var router = express.Router();
+
+router.get('/', function(req, res, next) {
+  return res.redirect('/users/signup');
+});
+
+
+router.get('/signup', function(req, res, next) {
+  res.render('signup', { title: 'Signup' });
+});
+
+
+router.post('/signup', function(req, res, next) {
+  const userid = req.body.userid;
+  if (!userid) {
+    return res.redirect('/users/signup');
+  }
+  return res.redirect('/users/' + encodeURIComponent(userid));
+});
+
+
+router.get('/:userid', function(req, res, next) {
+  const userid = req.params.userid;
+  if (!userid) {
+    return res.redirect('/users/signup');
+  }
+  res.render('welcome', { title: 'Welcome', userid: userid });
+});
+
+router.use(function routeMiddleware(req, res, next) {
+  res.render('route', { title: 'Users', routeName: 'users' });
+});
+
+module.exports = router;
